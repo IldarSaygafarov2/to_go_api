cee5105be2013d9a4d814fab7c3ab0ad932bb171
IldarSaygafarov2 <sayildar17@gmail.com>
2024-12-27 03:44:38 +0500

    added fixes

--- admin/main.py
+++ admin/main.py
@@ -3,10 +3,17 @@ from fastapi.templating import Jinja2Templates
 
 templates = Jinja2Templates(directory="templates")
 
-admin_router = APIRouter()
+admin_router = APIRouter(
+    prefix='/admin'
+)
 
 
-@admin_router.get('/admin/', name='admin')
+@admin_router.get('/', name='admin')
 async def show_admin_home_page(request: Request):
-    
+
     return templates.TemplateResponse('pages/index.html', {'request': request})
+
+
+@admin_router.get('/login/', name='login')
+async def admin_login(request: Request):
+    return templates.TemplateResponse('pages/login.html', {'request': request})
